8cf67532775a427571a77ca537cfeaab6cc09729
krunalrc <krunalchaudhar0909@gmail.com>
2024-07-03 17:03:36 +0530

    add

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import FirstStep from "../components/FirstStep.js";
-import Header from "../components/Header.js";
+import FirstStep from "../components/FirstStep";
+import Header from "../components/Header";
+import SecondStep from "../components/SecondStep";
+import ThirdStep from "../components/ThirdStep";
 
 const AppRouter = () => {
   <BrowserRouter>
@@ -9,6 +11,8 @@ const AppRouter = () => {
       <Header />
       <Switch>
         <Route component={FirstStep} path="/" exact={true} />
+        <Route component={SecondStep} path="/second" />
+        <Route component={ThirdStep} path="/third" />
       </Switch>
     </div>
   </BrowserRouter>;
